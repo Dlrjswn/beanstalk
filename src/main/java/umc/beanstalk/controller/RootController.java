1ebfdb5e7c8b8b3fdf13691690092a747f66044b
dlrjswn <lkj6387@gmail.com>
2024-06-19 19:01:50 +0900

    healthy check 추가

---
+++ src/main/java/umc/beanstalk/controller/RootController.java
@@ -0,0 +1,14 @@
+package umc.beanstalk.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+return "Im healthy";
+    }
+}
